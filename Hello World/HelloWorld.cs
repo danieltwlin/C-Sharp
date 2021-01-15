f03eaae2a733ee336c56a8ee161025b5d935bf3e
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-01-15 11:21:17 +0800

    Create HelloWorld.cs

---
+++ Hello World/HelloWorld.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+         
+            String str1 =Console.ReadLine();
+            Console.WriteLine(str1);
+            Console.ReadLine();
+        }
+    }
+}
