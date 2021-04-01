16074bccdb01adbc01c4c496527bb814a97779a5
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-04-01 18:04:49 +0800

    Create HttpClient_Demo.cs

---
+++ WebApi/HtteClient/HttpClient_Demo.cs
@@ -0,0 +1,18 @@
+static readonly HttpClient client = new HttpClient();
+
+static async Task Main()
+{
+  try	
+  {
+     HttpResponseMessage response = await client.GetAsync("http://www.google.com/");
+     response.EnsureSuccessStatusCode();
+     string responseBody = await response.Content.ReadAsStringAsync();
+     
+     Console.WriteLine(responseBody);
+  }
+  catch(HttpRequestException e)
+  {
+     Console.WriteLine("\nException Caught!");	
+     Console.WriteLine("Message :{0} ",e.Message);
+  }
+}
